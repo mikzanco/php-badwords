ab8443fcc96c55670257e6661d904155f282f7d0
mikzanco <mikyzancolo97@gmail.com>
2022-11-30 23:06:02 +0100

    add some css style to index.php

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Form</title>
 </head>
 <body>
  
-    <h1>Scrivi il testo che ti viene i mente e le parole da censurare</h1>
+    <h1>Scrivi il testo che ti viene in mente e le parole da censurare</h1>
 
     <form action="./pagina-di-atterraggio.php" method="POST">
         <input type="text" name="paragrafo" placeholder="scrivi un paragrafo" /> <br>
