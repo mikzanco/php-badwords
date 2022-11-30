369ce2297da151279f03c8b4205135d4d6d4234c
mikzanco <mikyzancolo97@gmail.com>
2022-11-30 15:16:18 +0100

    first commmit php project

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
     <title>Form</title>
 </head>
 <body>
-    <form action=""></form>
+ 
+    <h1>Scrivi il testo che ti viene i mente e le parole da censurare</h1>
+
+    <form action="./pagina-di-atterraggio.php" method="POST">
+        <input type="text" name="paragrafo" placeholder="scrivi un paragrafo" /> <br>
+        <input type="text" name="censura" placeholder="scrivi la parola da censurare" />
+        <button type="submit">invia</button>
+    </form>
 </body>
 </html>
---
+++ pagina-di-atterraggio.php
@@ -0,0 +1,20 @@
+<?php
+
+$testo = $_POST['paragrafo'];
+$cnsura = $_POST['censura'];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina di atterraggio</title>
+</head>
+<body>
+    <h1>Scrivi</h1>
+</body>
+</html>
